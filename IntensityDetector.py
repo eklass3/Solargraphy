b44a89df17d5f448dc51fba8fb10f12746988990
Eric Klassen <eklass3@uwo.ca>
2021-07-09 18:31:14 +0100

    Fixed colour channel clipping with values above 255

--- IntensityDetector.py
+++ IntensityDetector.py
@@ -27,25 +27,21 @@ def lowBurn(x):
 def burn(img, type):
     g = None
     if type == 1:
-        #g = numpy.vectorize(highBurn)
         b = img.point(highBurn) 
     elif type == 2:
-        g = numpy.vectorize(midBurn)
+        b = img.point(midBurn) 
     else:
-        #g = numpy.vectorize(lowBurn)
-         b = img.point(lowBurn)
-    
-    
-    #b = g(numpy.array(img))
-    
-    return numpy.array(b)
+        b = img.point(lowBurn)
+
+    # Save as 32 bit integer. Allows for values greater than 32 bits.
+    return numpy.array(b, dtype=numpy.uint32)
 
 
 camera = PiCamera()
 
 camera.start_preview()
 
-camera.shutter_speed = 1500 #
+camera.shutter_speed = 1000 #
 sleep(0.1)
 camera.capture('/home/pi/Solargraphy/assets/low.jpg')
 
@@ -57,31 +53,46 @@ camera.stop_preview()
 
 high = Image.open("/home/pi/Solargraphy/assets/high.jpg")
 low = Image.open("/home/pi/Solargraphy/assets/low.jpg")
-
+#Split channels
 redH, greenH, blueH = Image.Image.split(high)
 redL, greenL, blueL = Image.Image.split(low)
 
-arrRed = burn(redL, 3) + burn(redH, 1) # + burn(redM, 2) + burn(redL, 3)
-arrGreen = burn(greenL, 3) + burn(greenH, 1) # + burn(greenM, 2) + burn(greenL, 3)
-arrBlue = burn(blueL, 3) + burn(blueH, 1) # + burn(blueM, 2) + burn(blueL, 3)
+arrRed = burn(redL, 3) + burn(redH, 1)
+arrGreen = burn(greenL, 3) + burn(greenH, 1) 
+arrBlue = burn(blueL, 3) + burn(blueH, 1)
 
 if path.exists("/home/pi/Solargraphy/assets/greyScale.png"):
 
     og = Image.open("/home/pi/Solargraphy/assets/greyScale.png")
 
     ogRed, ogGreen, ogBlue = Image.Image.split(og)
+    
+    ogRed = numpy.array(ogRed)
+    ogGreen = numpy.array(ogGreen)
+    ogBlue = numpy.array(ogBlue)
+    
+    arrRed = numpy.add(arrRed, ogRed)
+    arrGreen = numpy.add(arrGreen, ogGreen)
+    arrBlue = numpy.add(arrBlue, ogBlue)
 
-    arrRed += ogRed
-    arrGreen += ogGreen
-    arrBlue += ogBlue
 
+print(numpy.amax(arrRed))
+# Force array values to be from 0-255 despite being 32bit.
+arrRed = numpy.clip(arrRed, 0, 255)
+arrGreen = numpy.clip(arrGreen, 0, 255)
+arrBlue = numpy.clip(arrBlue, 0, 255)
 
-finalRed = Image.fromarray(arrRed).convert("L")
-finalGreen = Image.fromarray(arrGreen).convert("L")
-finalBlue = Image.fromarray(arrBlue).convert("L")
+finalRed = Image.fromarray(arrRed)
+finalGreen = Image.fromarray(arrGreen)
+finalBlue = Image.fromarray(arrBlue)
 
+finalRed.save("/home/pi/Solargraphy/assets/redFinal.png")
+
+#Stack back channels
 rgb = numpy.dstack((finalRed, finalGreen, finalBlue))
-
+# Convert array type back to 8 bit (0-255)
+rgb = rgb.astype(numpy.uint8)
+# Create image, save
 imgFinal = Image.fromarray(rgb)
 imgFinal.save("/home/pi/Solargraphy/assets/greyScale.png")
 
